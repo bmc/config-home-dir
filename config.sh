0982af5c80e20bd71bce0a59d3153f01ce71d25d
Brian Clapper <bmc@clapper.org>
2016-06-22 10:22:49 -0400

    Commentary

--- config.sh
+++ config.sh
@@ -62,6 +62,8 @@ clone fish-config
 cd fish-config
 bash install.sh
 
+echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 
+echo "Installing Oh-My-Fish"
+curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
